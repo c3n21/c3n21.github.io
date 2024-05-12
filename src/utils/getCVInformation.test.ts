ae4c4ead688362c61628d1441c3387cf3157e372
c3n21 <chenzhifan9@gmail.com>
2024-05-12 10:40:47 +0200

    feat(vitest)

---
+++ src/utils/getCVInformation.test.ts
@@ -0,0 +1,14 @@
+import { expect } from 'vitest'
+import { getCVInformation } from './getCVInformation'
+
+const json_parse_spy = vi.spyOn(JSON, 'parse')
+import.meta.env.CV_JSON = '{}'
+
+test('getCVInformation', () => {
+    const cvInformation = getCVInformation()
+    expect(cvInformation).toStrictEqual({})
+    for (let i = 0; i < 10; i++) {
+        expect(getCVInformation()).toStrictEqual(cvInformation)
+    }
+    expect(json_parse_spy).toHaveBeenCalledTimes(1)
+})
