91350d396f1d1e34b220f63c204307a5d095cef4
c3n21 <chenzhifan9@gmail.com>
2024-05-18 10:51:51 +0200

    reset

--- src/schemas/CVInformation.ts
+++ src/schemas/CVInformation.ts
@@ -24,12 +24,25 @@ const contact = z.object({
 
 const project = z.object({})
 
+const technology = z.object({
+    name: z.string(),
+    domain: z.string(),
+})
+
+const workExperience = z.object({
+    company: z.string(),
+    description: z.string(),
+    period,
+    technologies: z.array(technology),
+})
+
 export const cvInformation = z.object({
     About: z.string(),
     'Skills & Knowledge': z.array(z.string()),
     Projects: z.array(project),
     Education: z.array(education),
     Contacts: z.array(contact),
+    WorkExperiences: z.array(workExperience),
 })
 
 export type CVInformation = z.infer<typeof cvInformation>
