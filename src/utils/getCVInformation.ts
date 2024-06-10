b23098b76689ca9f802d844a7d962fa9f23f91ef
c3n21 <chenzhifan9@gmail.com>
2024-06-10 08:48:27 +0200

    fix(getCVInformation): sonarlint warning

--- src/utils/getCVInformation.ts
+++ src/utils/getCVInformation.ts
@@ -3,7 +3,6 @@ import { cvInformation, type CVInformation } from '../schemas/CVInformation'
 let _cvInformation: CVInformation | null = null
 
 export function getCVInformation(): CVInformation {
-    return (_cvInformation ??= cvInformation.parse(
-        JSON.parse(import.meta.env.CV_JSON)
-    ))
+    _cvInformation ??= cvInformation.parse(JSON.parse(import.meta.env.CV_JSON))
+    return _cvInformation
 }
