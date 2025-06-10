12f8d5a16a99e8355b5060eb06ce593d2b8bccb7
c3n21 <37077738+c3n21@users.noreply.github.com>
2025-06-10 14:42:32 +0200

    update flake

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nvim.url = "path:/home/zhifan/.config/nvim";
 
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
@@ -9,44 +10,59 @@
     };
   };
 
-  outputs = inputs @ {
-    self,
-    nixpkgs,
-    flake-parts,
-  }:
-    flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      nvim,
+      flake-parts,
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
 
-      perSystem = {
-        system,
-        pkgs,
-        ...
-      }: {
-        devShells.default = pkgs.mkShell {
-          name = "nodejs-dev-shell";
-          buildInputs = with pkgs; [
-            nodejs_22 # Specify Node.js version
-            pnpm
-          ];
+      perSystem =
+        {
+          system,
+          pkgs,
+          ...
+        }:
+        let
+          nvim-pkgs = nvim.packages.${system};
+        in
+        {
+          devShells.default = pkgs.mkShell {
+            name = "nodejs-dev-shell";
+            buildInputs =
+              with pkgs;
+              [
+                nodejs_22 # Specify Node.js version
+                pnpm
+              ]
+              ++ (with nvim-pkgs; [ nastro ]);
 
-          shellHook = ''
-            echo "Welcome to the Node.js development environment using system {system}!"
-            echo "Node.js version: $(node --version)"
-          '';
+            shellHook = ''
+              echo "Welcome to the Node.js development environment using system ${system}!"
+              echo "Node.js version: $(node --version)"
+            '';
+          };
+
+          # Optionally define a package for the project
+          # packages.default = pkgs.stdenv.mkDerivation {
+          #   name = "nodejs-project";
+          #   src = ./.;
+          #
+          #   buildInputs = with pkgs; [nodejs-18_x];
+          #
+          #   installPhase = ''
+          #     mkdir -p $out
+          #     cp -r * $out
+          #   '';
+          # };
         };
-
-        # Optionally define a package for the project
-        # packages.default = pkgs.stdenv.mkDerivation {
-        #   name = "nodejs-project";
-        #   src = ./.;
-        #
-        #   buildInputs = with pkgs; [nodejs-18_x];
-        #
-        #   installPhase = ''
-        #     mkdir -p $out
-        #     cp -r * $out
-        #   '';
-        # };
-      };
     };
 }
