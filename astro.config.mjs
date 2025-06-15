b95428fea14f617897728531d1864c6b40e304f9
c3n21 <nonno.felice69uwu@gmail.com>
2025-06-15 11:46:12 +0200

    fix: use relative path for assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,10 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
     site: 'https://c3n21.github.io',
     base: 'personal-portfolio',
+    // opening index.html locally will break because it uses absolute path
+    build: {
+        assetsPrefix: './', // This makes asset paths relative
+    },
     vite: {
         plugins: [tailwindcss()],
     },
