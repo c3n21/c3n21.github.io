7a8d5d670de73277d98ed48509193295a17a7895
c3n21 <nonno.felice69uwu@gmail.com>
2025-06-15 11:13:50 +0200

    Merge pull request #3 from c3n21/feature/deploy-github-pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,11 @@ import tailwindcss from '@tailwindcss/vite'
 // https://astro.build/config
 export default defineConfig({
     site: 'https://c3n21.github.io',
-    // base: 'personal-portfolio',
+    base: 'personal-portfolio',
+    // opening index.html locally will break because it uses absolute path
+    build: {
+        assetsPrefix: './', // This makes asset paths relative
+    },
     vite: {
         plugins: [tailwindcss()],
     },
