231dd815ce409d896134e3139ac3f8f854f6c6f0
c3n21 <nonno.felice69uwu@gmail.com>
2025-06-22 15:27:26 +0200

    Rewamp Hero Section (#15)
    * feat(HeroSection): number of deployments
    
    * feat(workflows/deploy): include run_number
    
    * feat(HeroSection): use NeoVim LOC as statistics
    
    * fix(workflows/deploy): use correct format for NeoVim config repo
    
    * fix(workflows/deploy): correctly set env var
    
    * fix(workflows/deploy): correctly set CWD for NeoVim LOC

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+    readonly GITHUB_RUN_NUMBER: string
+    readonly NEOVIM_CONFIG_LINES: number
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv
+}
