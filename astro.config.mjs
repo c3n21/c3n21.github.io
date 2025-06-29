4ce8f20a69db87e50b9e297cc73eea8eef168d0f
c3n21 <nonno.felice69uwu@gmail.com>
2025-06-29 16:26:39 +0200

    feat: install qwik integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,20 @@ import { defineConfig } from 'astro/config'
 
 import tailwindcss from '@tailwindcss/vite'
 
+import qwikdev from '@qwikdev/astro';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://c3n21.github.io',
-    // opening index.html locally will break because it uses absolute path
-    build: {
-        assetsPrefix: './', // This makes asset paths relative
-    },
-    vite: {
-        plugins: [tailwindcss()],
-    },
+  site: 'https://c3n21.github.io',
+
+  // opening index.html locally will break because it uses absolute path
+  build: {
+      assetsPrefix: './', // This makes asset paths relative
+  },
+
+  vite: {
+      plugins: [tailwindcss()],
+  },
+
+  integrations: [qwikdev()],
 })
