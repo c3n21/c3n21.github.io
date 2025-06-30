eac2316442e3cfdf65ea71f27557172e9a924e37
c3n21 <nonno.felice69uwu@gmail.com>
2025-06-30 18:47:36 +0200

    fix(scripts/download_assets): don't rm

--- scripts/node/download_assets.ts
+++ scripts/node/download_assets.ts
@@ -1,13 +1,10 @@
-import fs, { mkdirSync, rmSync } from 'node:fs'
+import fs, { mkdirSync } from 'node:fs'
 import cv from '@/cv.json' assert { type: 'json' }
 import { Readable } from 'node:stream'
 import { finished } from 'node:stream/promises'
 import generateFileNameFromArtifact from '@/lib/linkedin/generateFileNameFromArtifact'
 
 const projectsDir = 'src/assets/projects'
-rmSync(projectsDir, {
-    recursive: true,
-})
 mkdirSync(projectsDir, { recursive: true })
 
 cv.projects.forEach((project, project_index) =>
