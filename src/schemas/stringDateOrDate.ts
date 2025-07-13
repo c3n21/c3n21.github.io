7054b93049408b7d22d6aab65156fe1a992f5310
c3n21 <nonno.felice69uwu@gmail.com>
2025-07-13 08:05:48 +0200

    chore: remove unused files

--- src/schemas/stringDateOrDate.ts
+++
@@ -1,7 +0,0 @@
-import { z } from 'astro/zod'
-
-export const stringDateOrDate = z
-    .string()
-    .datetime()
-    .or(z.date())
-    .transform((date) => (typeof date === 'string' ? new Date(date) : date))
--- src/utils/getCVInformation.ts
+++
@@ -1,8 +0,0 @@
-import { cvInformation, type CVInformation } from '../schemas/CVInformation'
-
-let _cvInformation: CVInformation | null = null
-
-export function getCVInformation(): CVInformation {
-    _cvInformation ??= cvInformation.parse(JSON.parse(import.meta.env.CV_JSON))
-    return _cvInformation
-}
